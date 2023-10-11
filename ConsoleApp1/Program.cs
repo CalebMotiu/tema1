c9692c78224f75db9b470457ae07e844d2d0c390
Caleb Motiu <caleb.motiu@emmanuel.ro>
2023-10-11 18:52:12 +0300

    inital project setup

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+        }
+
+ 
+    }
+}
